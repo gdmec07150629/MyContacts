9ea3f1c3ba031a17dd6c0124d07be7430519281d
lzj <827579668@qq.com>
2017-01-02 22:28:55 +0800

    29

--- app/src/main/java/cn/edu/gdmec/s07150629/mycontacts/MyDB.java
+++ app/src/main/java/cn/edu/gdmec/s07150629/mycontacts/MyDB.java
@@ -8,104 +8,121 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
- * Created by Administration on 2016/10/23.
+ * Created by Administrator on 2016/11/12.
  */
+//数据库类
 public class MyDB extends SQLiteOpenHelper{
-    private static String DB_NAME="My_DB.db";
-    private static int DB_VERSION=2;
+    private static String DB_NAME = "My_DB.db";
+    private static int DB_VERSION = 2;
     private SQLiteDatabase db;
+    //构造方法Context获取数据文件存放位置
+    public MyDB(Context context){
+        super(context,DB_NAME,null,DB_VERSION);
+        db = getWritableDatabase();
 
-    public MyDB(Context context) {
-        super(context, DB_NAME, null, DB_VERSION);
-        db=getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-    }
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
     }
 
-    public SQLiteDatabase openConnecction(){
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+    //打开数据库
+    public SQLiteDatabase openConnection(){
         if (!db.isOpen()){
-            db=getWritableDatabase();
+            db = getWritableDatabase();
         }
         return db;
     }
-
-    public void closeConnction(){
+    //关闭数据库
+    public void closeConnection(){
         try {
-            if (db != null && db.isOpen()) {
+            if (db!=null && db.isOpen()){
                 db.close();
             }
         }catch (Exception e){
             e.printStackTrace();
         }
     }
-
-    public boolean isTableExits(String tablename) {
-        try{
-            openConnecction();
-            String str="select count(*)xcount from"+tablename;
-            db.rawQuery(str,null).close();
-        }catch (Exception e){
-            e.printStackTrace();
-            return false;
-        }finally {
-            closeConnction();
-        }
-        return true;
-    }
-
-    public boolean createTable(String createTableSql) {
-        try{
-            openConnecction();
+    //创建数据库
+    public boolean createTable(String createTableSql){
+        try {
+            openConnection();
             db.execSQL(createTableSql);
-        }catch (SQLException e){
+        }catch(SQLException e){
             e.printStackTrace();
             return false;
         }finally {
-            closeConnction();
+            closeConnection();
         }
         return true;
     }
-
-    public boolean save(String tableName, ContentValues values) {
+    //保存数据
+    public boolean save(String tableName, ContentValues values){
         try{
-            openConnecction();
+            openConnection();
             db.insert(tableName,null,values);
         }catch (Exception e){
             e.printStackTrace();
             return false;
         }finally {
-            closeConnction();
+            closeConnection();
         }
         return true;
     }
-
+    //更新数据
     public boolean update(String table,ContentValues values,String whereClause,String[] whereArgs){
         try{
-            openConnecction();
+            openConnection();
             db.update(table,values,whereClause,whereArgs);
-        }catch (Exception e){
+        }catch(Exception e){
             e.printStackTrace();
             return false;
         }finally {
-            closeConnction();
+            closeConnection();
         }
         return true;
     }
-
+    //删除数据
+    public boolean delete(String table,String deleteSql,String obj[]){
+        try{
+            openConnection();
+            db.delete(table,deleteSql,obj);
+        }catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }finally{
+            closeConnection();
+        }
+        return true;
+    }
+    //查找数据
     public Cursor find(String findSql,String obj[]){
         try{
-            openConnecction();
-            Cursor cursor=db.rawQuery(findSql, obj);
+            openConnection();
+            Cursor cursor = db.rawQuery(findSql,obj);
             return cursor;
-        }catch (Exception e){
+        }catch(Exception e){
             e.printStackTrace();
             return null;
         }
     }
-
+    //判断数据表是否存在
+    public boolean isTableExits(String tablename){
+        try {
+            openConnection();
+            String str="select count(*)xcount from "+tablename;
+            db.rawQuery(str,null).close();
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }finally {
+            closeConnection();
+        }
+        return  true;
+    }
 }
--- app/src/main/java/cn/edu/gdmec/s07150629/mycontacts/User.java
+++ app/src/main/java/cn/edu/gdmec/s07150629/mycontacts/User.java
@@ -1,64 +1,67 @@
 package cn.edu.gdmec.s07150629.mycontacts;
 
 /**
- * Created by Administration on 2016/10/23.
+ * Created by Administrator on 2016/11/12.
  */
+//数据类
 public class User {
+    //字段名常量
     public final static String NAME = "name";
-    public static final String MOBILE = "mobile";
-    public static final String QQ = "qq";
-    public static final String DANWEI = "danwei";
-    public static final String ADDRESS = "address";
+    public final static String MOBILE = "mobile";
+    public final static String DANWEI = "danwei";
+    public final static String QQ = "qq";
+    public final static String ADDRESS = "address";
 
+    //类属性
     private String name;
     private String mobile;
-    private String qq;
     private String danwei;
+    private String qq;
     private String address;
-    private int id_DB=-1;
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getDanwei() {
-        return danwei;
-    }
-
-    public void setDanwei(String danwei) {
-        this.danwei = danwei;
+    private int id_DB =-1;
+    //getter seter 方法
+    public String getName() {
+        return name;
     }
 
     public String getMobile() {
         return mobile;
     }
 
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
+    public String getQq() {
+        return qq;
     }
 
-    public String getName() {
-        return name;
+    public String getDanwei() {
+        return danwei;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getId_DB() {
+        return id_DB;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public String getQq() {
-        return qq;
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
     }
 
     public void setQq(String qq) {
         this.qq = qq;
     }
 
-    public int getId_DB() {
-        return id_DB;
+    public void setDanwei(String danwei) {
+        this.danwei = danwei;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public void setId_DB(int id_DB) {
